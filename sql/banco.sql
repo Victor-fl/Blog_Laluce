a3dd1311e1d8878e806635ac2e1a0a3315620da3
Victor-fl <victorflaluce@gmail.com>
2025-08-27 11:59:53 -0300

    27/08/2025

--- sql/banco.sql
+++ sql/banco.sql
@@ -5,8 +5,8 @@ use Blog_Laluce;
 
 create table usuario
 (
-id int(11) not null auto_increment primary key,
-nome varchar not null(50),
+id int not null auto_increment primary key,
+nome varchar(50) not null,
 email varchar(225) not null,
 senha char(60) not null,
 data_criacao datetime not null default current_timestamp,
@@ -16,7 +16,7 @@ adm tinyint(4) not null default '0'
 
 create table post
 (
-id int (11) not null auto_increment primary key,
+id int not null auto_increment primary key,
 titulo varchar(225) not null,
 texto text not null,
 usuario_id int(11) not null,
